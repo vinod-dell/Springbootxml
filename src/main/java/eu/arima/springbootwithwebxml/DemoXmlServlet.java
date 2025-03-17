903fce1ebb5d32befef1e1e28f80d3f86f22fb14
vinod-dell <68218503+vinod-dell@users.noreply.github.com>
2025-03-17 16:40:25 +0530

    Add files via upload

---
+++ src/main/java/eu/arima/springbootwithwebxml/DemoXmlServlet.java
@@ -0,0 +1,25 @@
+package eu.arima.springbootwithwebxml;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DemoXmlServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        JSONArray pets = new JSONArray();
+        pets.put("cat");
+        pets.put("dog");
+        try {
+            Thread.sleep(80000);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        out.println(pets);
+        resp.getWriter().write("sleep 80Sec");
+    }
+}
